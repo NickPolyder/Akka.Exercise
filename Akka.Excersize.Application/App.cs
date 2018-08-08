8411e9cd2312ef67273977ad5823a1dc2f3709b3
Nikos Polideropoulos <nickpolyder@hotmail.gr>
2018-08-09 00:24:51 +0100

    Add project files.

---
+++ Akka.Excersize.Application/App.cs
@@ -0,0 +1,52 @@
+﻿using Akka.Actor;
+using Autofac;
+
+namespace Akka.Excersize.Application
+{
+    public class App
+    {
+        public IContainer Container { get; private set; }
+
+        public ActorSystem AkkaSystem => Container.Resolve<ActorSystem>();
+
+        private readonly string _appName;
+        public App()
+        { }
+
+        public App(string appName)
+        {
+            _appName = appName;
+        }
+
+        protected virtual void PreStart()
+        { }
+        
+        public void Start()
+        {
+            PreStart();
+            //TODO: Start
+            var containerBuilder = new ContainerBuilder();
+            ConfigureServices(containerBuilder);
+            Container = containerBuilder.Build();
+            PostStart();
+        }
+
+        protected virtual void PostStart()
+        { }
+
+        protected virtual void PreConfigureServices(ContainerBuilder containerBuilder)
+        {  }
+
+        protected void ConfigureServices(ContainerBuilder containerBuilder)
+        {
+            PreConfigureServices(containerBuilder);
+
+            containerBuilder.RegisterInstance(ActorSystem.Create(_appName));
+
+            PostConfigureServices(containerBuilder);
+        }
+
+        protected virtual void PostConfigureServices(ContainerBuilder containerBuilder)
+        { }
+    }
+}
