97e87620f5afd21c39077f44250d7552018ecaf1
Nikos Polideropoulos <nickpolyder@hotmail.gr>
2018-08-13 23:01:33 +0100

    [WIP] Reporting Service

---
+++ Akka.Exercise.Application/Services/Reporting/Messages/ReportMessage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Akka.Exercise.Application.Services.Reporting.Messages
+{
+    public class ReportMessage
+    {
+        public Guid RequestId { get; }
+
+        public string Source { get; }
+        public long ReceivedBytes { get; }
+        public long TotalBytes { get; }
+        public ReportMessage(string source,long receivedBytes,long totalBytes):this(Guid.NewGuid(),source,receivedBytes,totalBytes)
+        { }
+
+        public ReportMessage(Guid requestId,string source, long receivedBytes, long totalBytes)
+        {
+            RequestId = requestId;
+            Source = source;
+            ReceivedBytes = receivedBytes;
+            TotalBytes = totalBytes;
+
+        }
+    }
+}
